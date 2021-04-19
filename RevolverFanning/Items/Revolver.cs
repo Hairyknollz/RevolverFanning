879f132f47f21429017712659b24db4ead33170e
Jon Munro <Jonathan.m.munro@gmail.com>
2021-04-20 00:57:20 +0100

    Version 1.0

---
+++ RevolverFanning/Items/Revolver.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RevolverFanning.Items
+{
+    public class Revolver : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.Revolver);
+            item.autoReuse = true;
+            item.useAnimation = 48;
+            item.useTime = 8;
+            item.reuseDelay = 80;
+        }
+
+        //public override void AddRecipes()
+        //{
+        //    ModRecipe recipe = new ModRecipe(mod);
+        //    recipe.AddIngredient(ItemID.Wood);
+        //    recipe.AddTile(TileID.WorkBenches);
+        //    recipe.SetResult(this);
+        //    recipe.AddRecipe();
+        //}
+    }
+}
---
+++ RevolverFanning/RevolverFanning.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace RevolverFanning
+{
+	public class RevolverFanning : Mod
+	{
+	}
+}
